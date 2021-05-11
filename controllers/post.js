3782c154d55a4358bd8e81d01f347d8682560101
Mateusz Sobczyk <sobczyk.m@outlook.com>
2021-05-11 12:50:17 +0200

    GET posts returns author name

--- controllers/post.js
+++ controllers/post.js
@@ -2,16 +2,34 @@ const Post = require('../models/Post');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 exports.getPosts = (async (req, res) => {
-    const decoded = await jwt.verify(req.header('auth-token'), process.env.TOKEN_SECRET);
+    var users =[];
+    var usernames = [];
     try {
         const posts = await Post.find().exec();
-        const user = await User.findById(decoded);
-        res.json([posts, user.name]);
+        posts.map(elem =>{
+            users.push(elem.author);
+        });
+        try {
+            var itemsProcessed=0;
+            users.forEach(async user => {
+                const username = await User.findById(user).then( us => usernames.push(us.name)).then(() => console.log(usernames))
+                itemsProcessed++;
+                if(itemsProcessed === users.length) {
+                    res.json([posts, usernames]);
+                }   
+            })
+        } catch (error) {
+            res.json(error)
+        }
     } catch (error) {
         res.json(error);
     }
-
 });
+// users.forEach( async (user) =>{
+//     const userInfo = await User.findById(user).forEach( function(myDoc) { print( "user: " + myDoc.name ); });
+// });
+// console.log(users);
+// res.json(usernames);
 
 exports.getPost = (async (req, res) => {
     const decoded = await jwt.verify(req.header('auth-token'), process.env.TOKEN_SECRET);
